2106b4a67dac596e390728803dc91fedf193ac4f
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-04-15 17:59:25 -0600

    Change sample test and function.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-export const library = () => {
-  console.log('something')
+/**
+ * A test function that returns 1.
+ * 
+ * @example
+ * 1 === testFunction() // true
+ * 
+ * @param {*} [a_param] - Does nothing
+ */
+export const testFunction = (a_param) => {
   return 1;
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-import { library } from '../src/index';
+import { testFunction } from '../src/index';
 import test from 'tape';
 
 test('sample test', (t) => {
-  t.equal(1, library());
+  t.equal(1, testFunction());
   t.end();
 });
