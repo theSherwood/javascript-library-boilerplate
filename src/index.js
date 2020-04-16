d0396a2d9da5122d7bd7559a4ac73a597d990d69
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-04-16 07:48:10 -0600

    Configure linting, formatting, and ghooks.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/*eslint no-unused-vars: "off"*/
+
 /**
  * A test function that returns 1.
- * 
+ *
  * @example
  * 1 === testFunction() // true
- * 
+ *
  * @param {*} [a_param] - Does nothing
  */
 export const testFunction = (a_param) => {
   return 1;
-}
+};
