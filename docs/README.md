5ad97ce6c3c287775f62023ed8764988c41471f3
Adam Sherwood <adam.the.sherwood@gmail.com>
2020-04-15 18:00:19 -0600

    Add jsdocs.

---
+++ docs/README.md
@@ -0,0 +1,25 @@
+# API
+
+<a name="testFunction"></a>
+
+### testFunction
+
+A test function that returns 1.
+
+**Kind**: global constant
+
+| Param     | Type            | Description  |
+| --------- | --------------- | ------------ |
+| [a_param] | <code>\*</code> | Does nothing |
+
+**Example**
+
+```js
+1 === testFunction(); // true
+```
+
+---
+
+# Example
+
+TODO
--- jsdoc.config.js
+++ jsdoc.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  sourceType: "module",
+  sourceType: 'module',
   tags: {
     allowUnknownTags: true,
-    dictionaries: ["jsdoc"],
+    dictionaries: ['jsdoc'],
   },
-  files: "packages/*/src/**/*.js",
-  plugins: ["plugins/markdown"],
+  files: 'src/**/*.js',
+  plugins: ['plugins/markdown'],
   templates: {
     cleverLinks: false,
     monospaceLinks: true,
@@ -13,18 +13,18 @@ module.exports = {
     showInheritedInNav: true,
   },
   opts: {
-    destination: "./docs/",
-    encoding: "utf8",
+    destination: './docs/',
+    encoding: 'utf8',
     private: true,
     recurse: true,
   },
   // jsdoc2md
-  "heading-depth": 3,
-  "name-format": false,
+  'heading-depth': 3,
+  'name-format': false,
   separators: true,
-  "global-index-format": "grouped",
-  "member-index-format": "grouped",
-  "module-index-format": "grouped",
-  "param-list-format": "table",
-  "property-list-format": "table",
+  'global-index-format': 'grouped',
+  'member-index-format': 'grouped',
+  'module-index-format': 'grouped',
+  'param-list-format': 'table',
+  'property-list-format': 'table',
 };
